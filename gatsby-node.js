23cbcadabbf8eacf9fac0f02e2088305f9426d1b
nob3110 <saito@one-stone.co.jp>
2020-07-06 22:42:28 +0900

    gatsby version up

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,53 +1,5 @@
 const path = require('path')
 
-// exports.createPages = async ({ graphql, actions }) => {
-
-//   const { createPage } = actions
-//   const result = await graphql(`
-//     {
-//       allMicrocmsInterviews {
-//         edges {
-//           node {
-//             id
-//           }
-//         }
-//       }
-//       allMicrocmsOffers {
-//         edges {
-//           node {
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `)
-
-//   if (result.errors) {
-//     throw result.errors
-//   }
-
-//   result.data.allMicrocmsInterviews.edges.forEach(edge => {
-//     createPage({
-//       path: `/interviews/${edge.node.id}`,
-//       component: path.resolve('./src/templates/interview.js'),
-//       context: {
-//         id: edge.node.id,
-//       },
-//     })
-//   })
-
-//   result.data.allMicrocmsOffers.edges.forEach(edge => {
-//     createPage({
-//       path: `/offers/${edge.node.id}`,
-//       component: path.resolve('./src/templates/offer.js'),
-//       context: {
-//         id: edge.node.id,
-//       },
-//     })
-//   })
-// }
-
-
 // importを相対パス対応させる
 exports.onCreateWebpackConfig = ({stage, rules, loaders, plugins, actions}) => {
   actions.setWebpackConfig({
